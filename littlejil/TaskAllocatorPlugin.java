194c6f9b32631e89a9c89c099665830262195284
mp2079 <mp2079>
2002-11-19 23:40:17 +0000

    modified to just allocate a task (for the executor), without setting estimated results

--- littlejil/TaskAllocatorPlugin.java
+++ littlejil/TaskAllocatorPlugin.java
@@ -3,8 +3,7 @@ package psl.workflakes.littlejil;
 import org.apache.log4j.Logger;
 import org.cougaar.core.blackboard.IncrementalSubscription;
 import org.cougaar.core.plugin.ComponentPlugin;
-import org.cougaar.core.service.BlackboardService;
-import org.cougaar.core.service.DomainService;
+import org.cougaar.core.service.*;
 import org.cougaar.core.domain.RootFactory;
 import org.cougaar.util.UnaryPredicate;
 import org.cougaar.planning.ldm.plan.*;
@@ -12,6 +11,8 @@ import psl.workflakes.littlejil.xmlschema.*;
 import psl.workflakes.littlejil.xmlschema.types.*;
 
 import java.util.*;
+
+import psl.workflakes.littlejil.assets.*;
 
 /**
  * This class should get tasks posted on a blackboard and allocate them according to
@@ -25,13 +26,26 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
     private IncrementalSubscription leafTasksSubscription;
     private DomainService domainService;
     private RootFactory factory;
+    private PrototypeRegistryService prototypeRegistry;
 
     /**
      * Used by the binding utility through reflection to set my DomainService
      */
-    public void setDomainService(DomainService aDomainService) {
-        domainService = aDomainService;
-        factory = domainService.getFactory();
+    public void setDomainService(DomainService domainService) {
+        this.domainService = domainService;
+        factory = this.domainService.getFactory();
+
+    }
+
+    public void setPrototypeRegistryService(PrototypeRegistryService prototypeRegistryService) {
+        this.prototypeRegistry = prototypeRegistryService;
+
+        // create asset prototype
+        factory.addPropertyGroupFactory(new PropertyGroupFactory());
+        ExecAgentAsset prototype = (ExecAgentAsset) factory.createPrototype(ExecAgentAsset.class, "ExecAgentProto");
+
+        prototypeRegistry.cachePrototype("ExecAgent", prototype);
+
     }
 
     private class LeafTaskPredicate implements UnaryPredicate {
@@ -39,7 +53,7 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
             boolean ret = false;
             if (o instanceof Task) {
                 Task task = (Task) o;
-                ret = (task.getWorkflow() == null);     // leaf tasks have no workflow
+                ret = (task.getPlanElement() == null);     // leaf tasks have no PlanElement (expansion)
             }
             return ret;
         }
@@ -62,9 +76,35 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
 
             logger.debug("got task " + task.getVerb() + ", will allocate...");
 
-            // TODO: allocate, simulate result...
+            /*Preference end_time_pref = task.getPreference(AspectType.END_TIME);
+            if (end_time_pref == null) {
+                logger.debug("task has no end_time_pref!");
+                continue;
+            }
+
+            int end = (int) end_time_pref.getScoringFunction().getBest().getValue();
+
+            AllocationResult estAR = null;
+
+            // Create an estimate that reports that we did just what we were asked to do
+
+            boolean onTime = true;
+            int[] aspect_types = {AspectType.END_TIME};
+            double[] results = {end};
+            estAR = factory.newAllocationResult(1.0, //rating
+                    onTime, // success or not
+                    aspect_types,
+                    results);
+*/
+            ExecAgentAsset asset = (ExecAgentAsset) factory.createInstance("ExecAgent");
+            Allocation allocation = factory.createAllocation(task.getPlan(), task, asset, null, Role.ASSIGNED);
+
+            logger.info("publishing allocation for task " + task.getVerb());
+            blackboard.publishAdd(allocation);
+            blackboard.publishChange(task);
 
         }
+
 
     }
 
