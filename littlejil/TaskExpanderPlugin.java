5e7a33c0e3800430a64c2d96a4fe84181047a505
mp2079 <mp2079>
2002-11-19 23:40:53 +0000

    modified to properly identify when tasks end, started adding support for multiple constraints (eg from prerequisistes, but not quite working yet)

--- littlejil/TaskExpanderPlugin.java
+++ littlejil/TaskExpanderPlugin.java
@@ -2,7 +2,10 @@ package psl.workflakes.littlejil;
 
 import org.apache.log4j.Logger;
 import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.blackboard.Subscription;
+import org.cougaar.core.blackboard.CollectionSubscription;
 import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.plugin.util.PluginHelper;
 import org.cougaar.core.service.BlackboardService;
 import org.cougaar.core.service.DomainService;
 import org.cougaar.core.domain.RootFactory;
@@ -22,21 +25,20 @@ public class TaskExpanderPlugin extends ComponentPlugin {
 
     private static final Logger logger = Logger.getLogger(TaskExpanderPlugin.class);
     private DomainService domainService;
-    private RootFactory factory;
 
-    private IncrementalSubscription expansionsSubscriptions;
-    private IncrementalSubscription tasksSubscriptions;
-
+    private IncrementalSubscription expansionsSubscription;
+    private IncrementalSubscription tasksSubscription;
 
     /**
      * Used by the binding utility through reflection to set my DomainService
      */
-    public void setDomainService(DomainService aDomainService) {
-        domainService = aDomainService;
-        factory = domainService.getFactory();
+    public void setDomainService(DomainService domainService) {
+        this.domainService = domainService;
     }
 
-    private class ExpansionsPredicate implements UnaryPredicate {
+    private static class ExpansionsPredicate implements UnaryPredicate {
+        private final Logger logger = Logger.getLogger(ExpansionsPredicate.class);
+
         public boolean execute(Object o) {
             return (o instanceof Expansion);
         }
@@ -45,12 +47,12 @@ public class TaskExpanderPlugin extends ComponentPlugin {
     /**
      * This predicate only matches tasks that have a workflow (ie that are expandable)
      */
-    private class ExpandableTasksPredicate implements UnaryPredicate {
+    private static class ExpandableTasksPredicate implements UnaryPredicate {
         public boolean execute(Object o) {
             boolean ret = false;
             if (o instanceof Task) {
                 Task task = (Task) o;
-                ret = (task.getWorkflow() != null);
+                ret = (task.getPlanElement() != null);
             }
             return ret;
         }
@@ -58,69 +60,157 @@ public class TaskExpanderPlugin extends ComponentPlugin {
 
     public void setupSubscriptions() {
 
-        BlackboardService blackboardService = getBlackboardService();
-
         // now set up the subscription to get diagrams
-        expansionsSubscriptions = (IncrementalSubscription) blackboardService.subscribe(new ExpansionsPredicate());
-        tasksSubscriptions = (IncrementalSubscription) blackboardService.subscribe(new ExpandableTasksPredicate());
+        expansionsSubscription = (IncrementalSubscription) blackboard.subscribe(new ExpansionsPredicate());
+        tasksSubscription = (IncrementalSubscription) blackboard.subscribe(new ExpandableTasksPredicate());
 
 
     }
 
+    Vector exps = new Vector();
     public void execute() {
 
-        for (Enumeration tasks = tasksSubscriptions.getAddedList(); tasks.hasMoreElements();) {
+        /*for (Enumeration tasks = tasksSubscriptions.getAddedList(); tasks.hasMoreElements();) {
             Task task = (Task) tasks.nextElement();
             logger.debug("posting expansion for task " + task.getVerb());
 
             // post the expansion for this task
-            Expansion expansion = factory.createExpansion(task.getPlan(), task, task.getWorkflow(), null);
-            blackboard.publishAdd(expansion);
+            //Expansion expansion = factory.createExpansion(task.getPlan(), task, task.getWorkflow(), null);
+            blackboard.publishAdd(task.getPlanElement());
+
+        }*/
+
+        for (Enumeration expansions = expansionsSubscription.getAddedList(); expansions.hasMoreElements();) {
+            Expansion expansion = (Expansion) expansions.nextElement();
+            logger.debug("got new expansion " + expansion);
+
+            processExpansion(expansion, true);
 
         }
 
-        for (Enumeration expansions = expansionsSubscriptions.getAddedList(); expansions.hasMoreElements();) {
+        // look for changed expansions where the constraints were satisfied...
+        for (Enumeration expansions = expansionsSubscription.getChangedList(); expansions.hasMoreElements();) {
             Expansion expansion = (Expansion) expansions.nextElement();
-            logger.debug("looking at expansion " + expansion);
 
-            Workflow workflow = expansion.getWorkflow();
-            for (Enumeration subtasks = workflow.getTasks(); subtasks.hasMoreElements();) {
-                Task task = (Task) subtasks.nextElement();
-                logger.debug(" looking at task " + task.getVerb());
+            logger.debug("got changed expansion: " + expansion);
 
-                Enumeration constraints = workflow.getTaskConstraints(task);
-                if (!constraints.hasMoreElements()) {
-                    logger.debug("\tthis task has no constraints, publishing it");
-                    blackboard.publishAdd(task);
-                }
-                else {
-                    for (; constraints.hasMoreElements();) {
+            if (expansion.getEstimatedResult() != null) {
+                logger.info(">>>> task " + expansion.getTask().getVerb() + " is done <<<<");
+            }
+            else if (expansion.getReportedResult() == null) {
+                processExpansion(expansion, false);
+            }
+
+        }
+
+        for (Enumeration expansions = expansionsSubscription.elements(); expansions.hasMoreElements();) {
+
+            Expansion expansion = (Expansion) expansions.nextElement();
+            Workflow wf = expansion.getWorkflow();
+            Constraint constraint = wf.getNextPendingConstraint();
+
+            if (constraint != null) {
+                logger.info("found a pending constraint: " + PluginUtil.constraintToString(constraint));
+                ConstraintEvent ced = constraint.getConstrainedEventObject();
+                if (ced instanceof SettableConstraintEvent)
+                {
+                    ((SettableConstraintEvent)ced).setValue(constraint.computeValidConstrainedValue());
+
+                    Task constrainedTask = constraint.getConstrainedTask();
+
+                    // there could still have other constraints pending! (like prerequisistes)
+                    // FIX: this seems to work now, but when the second constraint is resolved we don't get it
+
+                    boolean remainingConstraints = false;
+                    for (Enumeration constraints = wf.getTaskConstraints(constrainedTask);constraints.hasMoreElements();) {
                         Constraint c = (Constraint) constraints.nextElement();
-                        logger.debug("\tfound a constraint: " + constraintToString(c));
-
-                        if (c.getConstrainedTask() != task) {
-                            logger.debug("\tthis task is not constrained, publishing it");
-                            blackboard.publishAdd(task);
+                        if (c.getConstrainedTask() == constrainedTask) {
+                            final double resultValue = c.getConstrainedEventObject().getResultValue();
+                            logger.debug("resultvalue=" + resultValue + ", value=" + c.getConstrainedEventObject().getValue());
+                            if (Double.isNaN(resultValue)) {
+                                logger.debug("one constraint satisfied, but there are still others not satisfied.");
+                                remainingConstraints = true;
+                                break;
+                            }
                         }
-                        else {
-                            logger.debug("\tconstraint exists, cannot publish this task at this time");
-                        }
+                    }
 
+                    if (remainingConstraints)
+                        continue;
+
+                    logger.info("publishing task " + constrainedTask.getVerb());
+                    blackboard.publishAdd(constrainedTask);
+
+                    PlanElement planElement = constrainedTask.getPlanElement();
+                    if (planElement != null && planElement instanceof Expansion) {
+                        logger.debug("publishing tasks's expansion:" + planElement);
+                        blackboard.publishChange(planElement);
                     }
                 }
             }
         }
 
-        // TODO: need to look for changed expansions were the constraints were satisfied...?
+        PluginHelper.updateAllocationResult(expansionsSubscription);
 
     }
 
-    private String constraintToString(Constraint c) {
+    private void processExpansion(Expansion expansion, boolean checkParentConstraints) {
+        // first check that the parent task for this expansion can be executed
+        Task parentTask = expansion.getTask();
+        Workflow parentWorkflow = parentTask.getWorkflow();
 
-        return "[Constraint: " + c.getConstrainedTask().getVerb() + " constrained by " +
-                c.getConstrainingTask().getVerb() +
-                ", aspect=" + AspectType.ASPECT_STRINGS[c.getConstrainingAspect()] + "]";
 
+        //logger.debug("expansion is for task " + parentTask + ", which has workflow " + parentWorkflow);
+        if (!checkParentConstraints)
+            logger.debug("[not checking parent task constraints]");
+
+        if (parentWorkflow != null && checkParentConstraints) {
+
+            Enumeration constraints = parentWorkflow.getTaskConstraints(parentTask);
+            while (constraints.hasMoreElements()) {
+                Constraint c = (Constraint) constraints.nextElement();
+                if (c.getConstrainedTask() == parentTask) {
+                    logger.debug("task has existing constraints, not expanding");
+                    return;
+                }
+            }
+        }
+
+        Workflow workflow = expansion.getWorkflow();
+        if (!workflow.getTasks().hasMoreElements()) {
+            logger.info("expansion " + expansion + " has no more tasks!");
+            return;
+        }
+
+        for (Enumeration subtasks = workflow.getTasks(); subtasks.hasMoreElements();) {
+            Task task = (Task) subtasks.nextElement();
+            //logger.debug("looking at task " + task.getVerb());
+
+            if (task.getPlanElement() == null) {
+                logger.debug("task " + task.getVerb() + " is a leaf task");
+
+                // have to check each constraint, because this task may just be constraining
+                // some other task. if we find that this task is indeed being constrained, then
+                // we stop and don't publish it yet
+                boolean constrained = false;
+                for (Enumeration constraints = workflow.getTaskConstraints(task); constraints.hasMoreElements();) {
+                    Constraint c = (Constraint) constraints.nextElement();
+                    logger.debug("  found a constraint: " + PluginUtil.constraintToString(c));
+
+                    if (c.getConstrainedTask() == task) {
+                        constrained = true;
+                        logger.debug("  constraint exists, cannot publish this task at this time");
+                        break;
+                    }
+                }
+                if (!constrained) {
+                    logger.debug("  this task is not constrained, publishing it");
+                    blackboard.publishAdd(task);
+                }
+
+
+            }
+        }
     }
 
 
