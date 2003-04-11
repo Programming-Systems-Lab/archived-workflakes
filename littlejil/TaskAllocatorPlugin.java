a5b8442d9ffe49720b97125d942140167ae1b2e9
mp2079 <mp2079>
2003-04-11 18:40:44 +0000

    added findExecAgent method (modeled after workflakes implementation) to find the appropriate exec agent for a task (assuming the task doesn't already specify a particular ExecAgent)

--- littlejil/TaskAllocatorPlugin.java
+++ littlejil/TaskAllocatorPlugin.java
@@ -4,10 +4,12 @@ import java.util.Enumeration;
 
 import org.apache.log4j.Logger;
 import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.blackboard.Subscription;
 import org.cougaar.core.domain.RootFactory;
 import org.cougaar.core.plugin.ComponentPlugin;
 import org.cougaar.core.service.*;
 import org.cougaar.planning.ldm.plan.*;
+import org.cougaar.planning.ldm.asset.Asset;
 import org.cougaar.util.UnaryPredicate;
 import psl.workflakes.littlejil.assets.*;
 
@@ -21,6 +23,8 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
 
     private static final Logger logger = Logger.getLogger(TaskAllocatorPlugin.class);
     private IncrementalSubscription leafTasksSubscription;
+    private IncrementalSubscription execAgentAssetsSubscription;
+
     private DomainService domainService;
     private RootFactory factory;
     private PrototypeRegistryService prototypeRegistry;
@@ -39,10 +43,20 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
 
         // create asset prototype
         factory.addPropertyGroupFactory(new PropertyGroupFactory());
-        ExecClassAgentAsset prototype = (ExecClassAgentAsset)
-                factory.createPrototype(ExecClassAgentAsset.class, "ExecClassAgentProto");
+        {
+            ExecClassAgentAsset prototype = (ExecClassAgentAsset)
+                    factory.createPrototype(ExecClassAgentAsset.class, "ExecClassAgentProto");
 
-        prototypeRegistry.cachePrototype("ExecClassAgent", prototype);
+            prototypeRegistry.cachePrototype("ExecClassAgent", prototype);
+        }
+
+        {
+            ExecWorkletAgentAsset prototype = (ExecWorkletAgentAsset)
+                    factory.createPrototype(ExecWorkletAgentAsset.class, "ExecWorkletAgentProto");
+
+            prototypeRegistry.cachePrototype("ExecWorkletAgent", prototype);
+        }
+
 
     }
 
@@ -57,12 +71,40 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
         }
     }
 
+    private class ExecAgentAssetPredicate implements UnaryPredicate {
+
+        public boolean execute(Object o) {
+            return (o instanceof ExecAgentAsset);
+        }
+    }
+
     public void setupSubscriptions() {
 
-        BlackboardService blackboardService = getBlackboardService();
+        leafTasksSubscription = (IncrementalSubscription) blackboard.subscribe(new LeafTaskPredicate());
+        execAgentAssetsSubscription = (IncrementalSubscription) blackboard.subscribe(new ExecAgentAssetPredicate());
 
-        // now set up the subscription to get leaf tasks
-        leafTasksSubscription = (IncrementalSubscription) blackboardService.subscribe(new LeafTaskPredicate());
+        // TEMPORARY: create exec agents
+        {
+            ExecClassAgentAsset asset = (ExecClassAgentAsset) factory.createInstance("ExecClassAgent");
+            NewExecutorPG executorPG = (NewExecutorPG) factory.createPropertyGroup("ExecutorPG");
+            executorPG.setCapabilities("any");
+
+            NewClassPG classPG = (NewClassPG) factory.createPropertyGroup("ClassPG");
+            classPG.setClassName("psl.workflakes.littlejil.TaskExecutorInternalPlugin$DummyExecutableTask");
+            asset.setExecutorPG(executorPG);
+            asset.setClassPG(classPG);
+
+            blackboard.publishAdd(asset);
+        }
+
+        /*{
+            ExecWorkletAgentAsset asset = (ExecWorkletAgentAsset) factory.createInstance("ExecWorkletAgent");
+            NewExecutorPG executorPG = (NewExecutorPG) factory.createPropertyGroup("ExecutorPG");
+            executorPG.setCapabilities("any");
+            asset.setExecutorPG(executorPG);
+
+            blackboard.publishAdd(asset);
+        }*/
 
     }
 
@@ -71,24 +113,50 @@ public class TaskAllocatorPlugin extends ComponentPlugin {
         for (Enumeration tasks = leafTasksSubscription.getAddedList(); tasks.hasMoreElements();) {
             Task task = (Task) tasks.nextElement();
 
-            // TODO: use a real execution agent
-            // TESTING
-            ExecClassAgentAsset asset = (ExecClassAgentAsset) factory.createInstance("ExecClassAgent");
-            NewExecutorPG executorPG = (NewExecutorPG) factory.createPropertyGroup("ExecutorPG");
-            NewClassPG classPG = (NewClassPG) factory.createPropertyGroup("ClassPG");
-            classPG.setClassName("psl.workflakes.littlejil.TaskExecutorInternalPlugin$DummyExecutableTask");
-            asset.setExecutorPG(executorPG);
-            asset.setClassPG(classPG);
+            // find an execution asset for this task.
+            ExecAgentAsset asset = null;
+            try {
+                asset = findExecAgent(task);
+                Allocation allocation = factory.createAllocation(task.getPlan(), task, asset, null, Role.ASSIGNED);
 
-            Allocation allocation = factory.createAllocation(task.getPlan(), task, asset, null, Role.ASSIGNED);
+                logger.info("publishing allocation for task " + task.getVerb());
+                blackboard.publishAdd(allocation);
+                blackboard.publishChange(task);
 
-            logger.info("publishing allocation for task " + task.getVerb());
-            blackboard.publishAdd(allocation);
-            blackboard.publishChange(task);
+            } catch (PluginException e) {
+                logger.warn("Could not find asset for task " + task.getVerb() + ": " + e);
+                logger.warn("Not publishing task");
+            }
+
 
         }
 
 
+    }
+
+    /**
+     * Returns an appropriate execution agent asset for this task.
+     * This implementation simply finds the first matching asset.
+     * It can be overriden by a subclass as necessary
+     *
+     * TODO: specify agent asset as part of Step def?
+     * @param task the task to find the Execution Agent asset for
+     * @return an asset representing the Execution Agent
+     * @throws PluginException if an appropriate asset cannot be found
+     */
+    protected ExecAgentAsset findExecAgent(Task task) throws PluginException {
+
+        // iterate through available assets, return first that matches
+        for (Enumeration assets = execAgentAssetsSubscription.elements(); assets.hasMoreElements();) {
+            ExecAgentAsset asset = (ExecAgentAsset) assets.nextElement();
+            String capabilities = asset.getExecutorPG().getCapabilities();
+            if (capabilities.equals("any") ||
+                capabilities.equals(task.getVerb())) {
+                return asset;
+            }
+        }
+
+        throw new PluginException("No matching ExecAgent assets found");
     }
 
 
